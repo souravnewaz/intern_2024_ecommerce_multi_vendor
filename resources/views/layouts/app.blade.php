0566b757b5741b16b1797384fb65149acb5ad618
Rakibul Newaz Sourav <rakibulnewaz@gmail.com>
2024-09-05 11:16:04 +0600

    footer hidden

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,11 +31,11 @@
         @yield('content')
     </div>
 
-    <footer class="py-3 bg-dark">
+    <!-- <footer class="py-3 bg-dark">
         <div class="container">
             <p class="m-0 text-center text-white">Copyright &copy; {{ env('APP_NAME') }} 2024</p>
         </div>
-    </footer>
+    </footer> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 

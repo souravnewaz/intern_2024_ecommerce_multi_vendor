1e9aa79debbbc5540d45d78082deec5dd4e3f2fd
Rakibul Newaz Sourav <rakibulnewaz@gmail.com>
2024-09-05 11:15:29 +0600

    login page updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,28 +1,26 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="row d-flex justify-content-center">
-    <div class="col-md-8 col-lg-4">
-        <div class="card my-5">
-            <div class="card-header">
-                <h5 class="card-title">Login</h5>
-            </div>
-            <div class="card-body">
-                <form action="{{ route('login') }}" method="POST">
-                    @csrf
-                    <div class="mb-3">
-                        <label>Email</label>
-                        <input type="email" class="form-control" name="email">
-                    </div>
-                    <div class="mb-3">
-                        <label>Password</label>
-                        <input type="password" class="form-control" name="password">
-                    </div>
+<div class="h-100 d-flex align-items-center justify-content-center">
+    <div class="card col-md-8 col-lg-4">
+        <div class="card-header">
+            <h5 class="card-title">Login</h5>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('login') }}" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <label>Email</label>
+                    <input type="email" class="form-control" name="email">
+                </div>
+                <div class="mb-3">
+                    <label>Password</label>
+                    <input type="password" class="form-control" name="password">
+                </div>
 
-                    @include('components.alerts')
-                    <button type="submit" class="btn btn-primary">Login</button>
-                </form>
-            </div>
+                @include('components.alerts')
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,11 +11,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <link href="/assets/css/styles.css" rel="stylesheet" />
     <style>
-        body {
-            min-height: 100vh;
+        * {
+            padding: 0;
+            margin: 0;
         }
-        .content {
-            min-height: 70vh;
+        html,
+        body, .content{
+            min-height: 100% !important;
+            height: 100%;
         }
     </style>
 </head>
@@ -24,11 +27,11 @@
 
     @include('layouts.navbar')
 
-    <div class="content">
+    <div class="content container-fluid">
         @yield('content')
     </div>
 
-    <footer class="py-5 bg-dark">
+    <footer class="py-3 bg-dark">
         <div class="container">
             <p class="m-0 text-center text-white">Copyright &copy; {{ env('APP_NAME') }} 2024</p>
         </div>

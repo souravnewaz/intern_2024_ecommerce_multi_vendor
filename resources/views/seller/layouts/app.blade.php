00448cbfbb79a671ac8fbc6501c1d7b456824349
Rakibul Newaz Sourav <rakibulnewaz@gmail.com>
2024-09-22 12:26:38 +0600

    flash message updated

--- resources/views/seller/layouts/app.blade.php
+++ resources/views/seller/layouts/app.blade.php
@@ -1,35 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Dashboard</title>
-        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-        <link href="{{ asset('assets/seller/css/styles.css') }}" rel="stylesheet" />
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-    </head>
-    <body class="sb-nav-fixed">
-        @include('seller.layouts.navbar')
-        <div id="layoutSidenav">
-            @include('seller.layouts.sidebar')
-            <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid p-3">
-                        @yield('content')
-                    </div>
-                </main>
-                @include('seller.layouts.footer')
-            </div>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="{{ asset('assets/seller/js/scripts.js') }}"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-        <script src="{{ asset('assets/seller/js/datatables-simple-demo.js') }}"></script>
 
-        @stack('scripts')
-    </body>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+    <link href="{{ asset('assets/seller/css/styles.css') }}" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    @include('seller.layouts.navbar')
+    <div id="layoutSidenav">
+        @include('seller.layouts.sidebar')
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid p-3">
+                    @if ($message = Session::get('success'))
+                    <div class="alert alert-success alert-block">
+                        <strong>{{ $message }}</strong>
+                    </div>
+                    @endif
+                    @if ($message = Session::get('error'))
+                    <div class="alert alert-danger alert-block">
+                        <strong>{{ $message }}</strong>
+                    </div>
+                    @endif
+                    @if ($message = Session::get('warning'))
+                    <div class="alert alert-warning alert-block">
+                        <strong>{{ $message }}</strong>
+                    </div>
+                    @endif
+                    @if ($message = Session::get('info'))
+                    <div class="alert alert-info alert-block">
+                        <strong>{{ $message }}</strong>
+                    </div>
+                    @endif
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach($errors->getMessages() as $key => $value)
+                            <li>{{ $value[0] }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
+
+                    @yield('content')
+                </div>
+            </main>
+            @include('seller.layouts.footer')
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="{{ asset('assets/seller/js/scripts.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+    <script src="{{ asset('assets/seller/js/datatables-simple-demo.js') }}"></script>
+
+    @stack('scripts')
+</body>
+
 </html>
